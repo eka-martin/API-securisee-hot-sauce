2f59c43d8d4fa511f3c5f3f791a23e94430be5e3
eka-martin <99816833+eka-martin@users.noreply.github.com>
2022-08-26 16:34:01 +0200

    corrige d'erreur/image

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -9,6 +9,10 @@ exports.createSauce = (req, res, next) => {
     const sauce = new Sauce({
         ...sauceObject,
         userId: req.auth.userId,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: [],
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     sauce.save()
